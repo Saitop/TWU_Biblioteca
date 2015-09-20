4ed0af54813e0d35b9781431cc3d2725aaffb67d
Hanxian Lin <saitoplam@hotmail.com>
2015-09-20 20:10:11 +0800

    add parameter id in books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -19,9 +19,9 @@ public class BibliotecaApp {
 
     private static void showAllBooksInLibrary() {
         List<Book> books = new ArrayList<Book>();
-        Book theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1925");
-        Book refactoring = new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "1999");
-        Book tdd = new Book("Test-Driven Development by Example", "Kent Beck", "2003");
+        Book theGreatGatsby = new Book(1, "The Great Gatsby", "F. Scott Fitzgerald", "1925");
+        Book refactoring = new Book(2,"Refactoring: Improving the Design of Existing Code", "Martin Fowler", "1999");
+        Book tdd = new Book(3, "Test-Driven Development by Example", "Kent Beck", "2003");
 
         books.add(theGreatGatsby);
         books.add(refactoring);
--- test/com/twu/biblioteca/BookTests.java
+++ test/com/twu/biblioteca/BookTests.java
@@ -11,9 +11,9 @@ import static org.junit.Assert.*;
 public class BookTests {
     @Test
     public void shouldGetBookDetails(){
-        Book theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1925");
-        Book refactoring = new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "1999");
-        Book tdd = new Book("Test-Driven Development by Example", "Kent Beck", "2003");
+        Book theGreatGatsby = new Book(1, "The Great Gatsby", "F. Scott Fitzgerald", "1925");
+        Book refactoring = new Book(2,"Refactoring: Improving the Design of Existing Code", "Martin Fowler", "1999");
+        Book tdd = new Book(3, "Test-Driven Development by Example", "Kent Beck", "2003");
         String theGreatGatsbyDetail = theGreatGatsby.getDetails();
         String refactoringDetail = refactoring.getDetails();
         String tddDetail = tdd.getDetails();
