f5d79e6bffe4bba353173fe5d6fad418c63b0880
Hanxian Lin <saitoplam@hotmail.com>
2015-09-20 17:47:25 +0800

    Refactor - extract method: welcome info

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,10 +3,12 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+        welcomeInfo();
+    }
+
+    private static void welcomeInfo() {
         System.out.println("=====================================================");
         System.out.println("Welcome to Bibiloteca! Enjoy you adventure in books!");
         System.out.println("=====================================================");
-
-
     }
 }
