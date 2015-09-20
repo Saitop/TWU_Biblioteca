99aec23fb728b59beddac3b9a6d91a373a5f8a0d
Hanxian Lin <saitoplam@hotmail.com>
2015-09-20 17:26:35 +0800

    implement welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("=====================================================");
+        System.out.println("Welcome to Bibiloteca! Enjoy you adventure in books!");
+        System.out.println("=====================================================");
+
+
     }
 }
