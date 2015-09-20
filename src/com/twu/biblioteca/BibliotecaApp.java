c58b5d79ede3fa837243147dd147d0310cc5a87f
Hanxian Lin <saitoplam@hotmail.com>
2015-09-20 19:39:51 +0800

    Refactoring - extract method show books list

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -7,13 +7,16 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         welcomeInfo();
+        showAllBooksInLibrary();
     }
 
     private static void welcomeInfo() {
         System.out.println("=====================================================");
         System.out.println("Welcome to Bibiloteca! Enjoy you adventure in books!");
         System.out.println("=====================================================");
+    }
 
+    private static void showAllBooksInLibrary() {
         List<Book> books = new ArrayList<Book>();
         Book harryPotter = new Book("Harry Potter", "J.K. Rowling","1997");
         books.add(harryPotter);
